a91f05fa9317ee58be765889dedba60d456366c2
Artyom Sushchenko <artoymsushchenko@gmail.com>
2024-03-28 23:45:52 +0300

    edit cors for live server

--- src/main/java/com/sushchenko/mystictourismapp/security/SecurityConfig.java
+++ src/main/java/com/sushchenko/mystictourismapp/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5500"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5500", "http://127.0.0.1:5500"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
